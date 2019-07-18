ce3e3f73a00ea2f2ce13f1b277ce675bc5132e38
wmxhmx <wmxhmx@foxmail.com>
2019-07-18 18:46:33 +0800

    新增打印

--- gittest/src/main/java/com/itheima/HelloWorld.java
+++ gittest/src/main/java/com/itheima/HelloWorld.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("元芳你怎么看");
     }
 }
